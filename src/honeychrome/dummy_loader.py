674dce4bf420f53f46a9e7857faabb9c2fb6e54c
Salman Samson Rogers <1145601+salmansamson@users.noreply.github.com>
2026-05-15 18:34:17 +0100

    fixed dummy loader

--- src/honeychrome/dummy_loader.py
+++ src/honeychrome/dummy_loader.py
@@ -3,12 +3,16 @@ import sys
 class Mock(object):
     def __init__(self, *args, **kwargs):
         pass
+
     def __call__(self, *args, **kwargs):
         return Mock()
-    @classmethod
-    def __getattr__(cls, name):
+
+    def __getattr__(self, name):
+        # Prevent infinite recursion on private/internal attributes
+        if name.startswith('__'):
+            raise AttributeError(name)
         return Mock()
 
 # Define the "blocked" modules
-for mod in ['bokeh', 'bokeh.plotting', 'bokeh.models', 'bokeh.layouts', 'scipy.stats', 'scipy.interpolate', 'scipy.fft', 'narwhals']:
+for mod in ['bokeh', 'bokeh.plotting', 'bokeh.models', 'bokeh.layouts', 'narwhals']:
     sys.modules[mod] = Mock()
