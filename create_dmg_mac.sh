99d736fb5731b788a28b16dd610ee71235c55897
Salman Samson Rogers <1145601+salmansamson@users.noreply.github.com>
2026-05-23 06:58:12 +0100

    dmg build with several stapling attempts

--- create_dmg_mac.sh
+++ create_dmg_mac.sh
@@ -55,10 +55,6 @@ MAX_RETRIES=5
 COUNT=0
 SUCCESS=false
 
-# Explicitly check status before attempting to staple
-echo "Verifying notarization status..."
-xcrun notarytool info <SUBMISSION_ID> --apple-id "$APPLE_ID" --password "$APPLE_APP_PASSWORD" --team-id "$APPLE_TEAM_ID"
-
 while [ $COUNT -lt $MAX_RETRIES ]; do
   echo "Attempting to staple... (Attempt $((COUNT+1))/$MAX_RETRIES)"
   if xcrun stapler staple "$DMG_NAME"; then
